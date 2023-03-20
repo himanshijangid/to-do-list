3cd21b5ead6b5647f94c3f9f275217a77e31747f
Himanshi Jangid <himanshijangid444@gmail.com>
2023-03-20 14:03:09 +0530

    ggf

---
+++ script.js
@@ -0,0 +1,48 @@
+document.querySelector("#input").addEventListener("keydown", (event) => {
+    if (event.key ==="Enter"){
+        const input = document.querySelector("#input");
+        addItem(input.value);
+    }
+});
+
+document.querySelector("#add_item").addEventListener("click", () => {
+    const input = document.querySelector("#input");
+    addItem(input.value);
+});
+addItem  = (input) => {
+    const item = document.createElement("div");
+    const div = document.createElement("div");
+    const checkboxIcon = document.createElement("i");
+    const deleteIcon = document.createElement("i");
+    const text = document.createElement("p");
+
+    item.className = "item";
+    text.textContent = input;
+
+    checkboxIcon.className ="fa-regular fa-circle-check";
+    checkboxIcon.style.color = "red" ;
+
+    function checkbox (){
+        if(checkboxIcon.style.color = "red"){
+            checkboxIcon.style.color = "green";
+        }if (checkboxIcon.style.color = "green"){
+            checkboxIcon.style.color = "red";
+        }
+    }
+    checkboxIcon.addEventListener("click" , checkbox() );
+    div.appendChild(checkboxIcon);
+
+    deleteIcon.className ="fa-solid fa-trash-can";
+    deleteIcon.addEventListener("click",() => {
+        item.remove();
+
+    })
+    div.appendChild(deleteIcon);
+    item.appendChild (text);
+    item.appendChild(div);
+
+    document.querySelector("#to-do-list").appendChild(item);
+    document.querySelector("#input").value = "";
+
+
+}
